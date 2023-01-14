0593e4e6fc9dcdbb98228a4b99afafe4d40c1bda
RaulCruzG <rcruzg004@alumno.uaemex.mx>
2023-01-14 15:55:37 -0600

    prueba del repo

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Menu = () => {
-  return <div className="text-center text-fuchsia-600">Menu</div>;
+  return <div className="text-center text-fuchsia-600">Menu por parte del rulo</div>;
 };
 
 export default Menu;
