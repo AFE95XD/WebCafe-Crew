0d037b79c163e9296e486c67424f987c5395295b
AFE <afe95xd@gmail.com>
2023-01-14 15:35:17 -0600

    Inicializando el repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Components/Menu";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl text-lime-400">HOLA MUNDO</h1>
+      <Menu />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <div className="text-center text-fuchsia-600">Menu</div>;
+};
+
+export default Menu;
